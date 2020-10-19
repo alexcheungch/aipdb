db512229133e68a00099b66c846ece65a9dc97d3
alexcheungch <41729071+alexcheungch@users.noreply.github.com>
2020-10-19 22:12:27 +0800

    Update to use 'left' join

--- application/models/JobProg_model.php
+++ application/models/JobProg_model.php
@@ -11,8 +11,8 @@ class JobProg_model extends MY_Basemodel {
 
     public function getfullJob($jobcode)
     {
-    return $this->join('JobMtn', 'JobMtn.JobCode = JobProg.JobCode') 
-         ->join('ClientMtn', 'JobMtn.ClientCode1 = ClientMtn.ClientCode1')
+    return $this->join('JobMtn', 'JobMtn.JobCode = JobProg.JobCode', 'left') 
+         ->join('ClientMtn', 'JobMtn.ClientCode1 = ClientMtn.ClientCode1', 'left')
          ->where('JobCode', $jobcode)
          ->get();
     }
